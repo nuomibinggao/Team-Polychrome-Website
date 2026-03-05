015505c0ab99764db7e34beb36a6035a2ed69f88
nuomibinggao <Sicheng_Guide@hotmail.com>
2026-03-05 19:35:10 +0800

    finished translations for /resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { _ } from 'svelte-i18n';
-	import { resolve } from '$app/paths';
+  import { resolve } from '$app/paths';
   import { toggleLang } from '$lib/i18n/toggle';
 </script>
 
@@ -15,16 +15,20 @@
 
 <nav>
   <ul>
-    <li><a href={resolve('/')} class='pages'>{$_('nav.back_home')}</a></li>
+    <li>
+      <a href={resolve('/')} class='pages'>{$_('nav.back_home')}</a>
+    </li>
 
-    <li><a href={null} onclick={(e) => { e.preventDefault(); toggleLang(); }} class='i18n'>简中 / EN</a></li>
+    <li>
+      <a href={null} onclick={(e) => { e.preventDefault(); toggleLang(); }} class='i18n'>简中 / EN</a>
+    </li>
   </ul>
 </nav>
 
 <section class='intro'>
   <div class='content'>
     <h2>{$_('pages.resources.heading')}</h2>
-      <p>{$_('pages.resources.intro_text')}</p>
+    <p>{$_('pages.resources.intro_text')}</p>
     <h3>{$_('pages.about.useful_links')}</h3>
     <ul>
       <li>
@@ -39,28 +43,37 @@
 
 <section class='prettier-tags'>
   <div class='content'>
-    <h2>Prettier Tags</h2>
-    <p>Prettier Tags{$_('pages.resources.prettier_tags_desc_1')}<a href='https://github.com/modlist-org/Overlayer' class='inline-link'>Overlayer</a>{$_('pages.resources.prettier_tags_desc_2')}<a href='https://github.com/modlist-org/Overlayer.Scripting' class='inline-link'>Overlayer.Scripting</a>{$_('pages.resources.prettier_tags_desc_3')}</p>
-    <p>{$_('pages.resources.prettier_tags_desc_4')}<strong>nuomibinggao</strong>{$_('pages.resources.prettier_tags_desc_5')}<strong>MLob_302</strong>{$_('pages.resources.prettier_tags_desc_6')}</p>
+    <h2>{$_('pages.resources.prettier_tags.heading')}</h2>
+    <p>{$_('pages.resources.prettier_tags.desc_1')}<a href='https://github.com/modlist-org/Overlayer' class='inline-link'>Overlayer</a>{$_('pages.resources.prettier_tags.desc_2')}<a href='https://github.com/modlist-org/Overlayer.Scripting' class='inline-link'>Overlayer.Scripting</a>{$_('pages.resources.prettier_tags.desc_3')}
+    </p>
+    <p>
+      {$_('pages.resources.prettier_tags.desc_4')}
+      <strong>nuomibinggao</strong>{$_('pages.resources.prettier_tags.desc_5')}
+      <strong>MLob_302</strong>{$_('pages.resources.prettier_tags.desc_6')}
+    </p>
   </div>
   <div class='links'>
     <a href='https://github.com/nuomibinggao/Prettier-Tags' rel='noopener noreferrer' target='_blank'>{$_('pages.resources.github_link')}</a>
   </div>
 </section>
 
-<section class='nuomibinggao-kv'>
+<section class='keyviewer'>
   <div class='content'>
-    <h2>nuomibinggao's KeyViewer Files</h2>
-    <p>A collection of modern minimal style KeyViewer profiles made for the KeyViewer mod.</p>
-    <p>Made by team member <strong>nuomibinggao</strong>.</p>
+    <h2>{$_('pages.resources.keyviewer.heading')}</h2>
+    <p>{$_('pages.resources.keyviewer.desc_1')}</p>
+    <p>{$_('pages.resources.keyviewer.desc_2')}<strong>nuomibinggao</strong>{$_('pages.resources.keyviewer.desc_3')}
+    </p>
   </div>
+
   <div class='links'>
-    <a href='https://www.bilibili.com/video/BV1xRHZz7Eeq' rel='noopener noreferrer' target='_blank'>View Demo Video</a>
-    <a href={null}>Download Files</a>
+    <a href='https://www.bilibili.com/video/BV1xRHZz7Eeq' rel='noopener noreferrer' target='_blank'>{$_('pages.resources.keyviewer.demo_video')}</a>
+    <a href={null}>{$_('pages.resources.keyviewer.download')}</a>
   </div>
 </section>
 
 <footer>
-  &copy; 2026 nuomibinggao & Team Polychrome. Licensed Under CC BY-NC-ND 4.0.
-  <div class='footer-splash'>Even errors can look good.</div>
+  {$_('footer.copyright')}
+  <div class='footer-splash'>
+    {$_('footer.splash')}
+  </div>
 </footer>
