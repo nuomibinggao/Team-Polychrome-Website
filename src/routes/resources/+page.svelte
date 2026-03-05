7c4b1ed227a06d373785d4b9de87cf8c066a1bcb
nuomibinggao <Sicheng_Guide@hotmail.com>
2026-03-05 19:20:08 +0800

    started translation for /resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,20 +1,21 @@
 <script>
-  // import { _ } from 'svelte-i18n';
+  import { _ } from 'svelte-i18n';
 	import { resolve } from '$app/paths';
   import { toggleLang } from '$lib/i18n/toggle';
 </script>
+
 <svelte:head>
-  <title>Team Polychrome | Tools & Resources</title>
+  <title>{$_('meta.title')} | {$_('pages.resources.heading')}</title>
 </svelte:head>
 
 <h1>
-  <img src='images/placeholder.png' alt="Team Polychrome Logo" class='team-logo'>
-  Tools & Resources
+  <img src='images/placeholder.png' alt={$_('h1.logo_alt')} class='team-logo'>
+  {$_('pages.resources.heading')}
 </h1>
 
 <nav>
   <ul>
-    <li><a href={resolve('/')} class='pages'>Back to Home</a></li>
+    <li><a href={resolve('/')} class='pages'>{$_('nav.back_home')}</a></li>
 
     <li><a href={null} onclick={(e) => { e.preventDefault(); toggleLang(); }} class='i18n'>简中 / EN</a></li>
   </ul>
@@ -22,15 +23,15 @@
 
 <section class='intro'>
   <div class='content'>
-    <h2>Tools & Resources</h2>
-      <p>Explore the tools and resources our team members created for the ADOFAI community.</p>
-    <h3>Useful Links</h3>
+    <h2>{$_('pages.resources.heading')}</h2>
+      <p>{$_('pages.resources.intro_text')}</p>
+    <h3>{$_('pages.about.useful_links')}</h3>
     <ul>
       <li>
-        <a href='https://tuforums.com' rel='noopener noreferrer' target='_blank'>The Universal Forums (TUF)</a>
+        <a href='https://tuforums.com' rel='noopener noreferrer' target='_blank'>{$_('pages.about.tuf_link')}</a>
       </li>
       <li>
-        <a href='https://github.com/modlist-org' rel='noopener noreferrer' target='_blank'>modlist.org on GitHub</a>
+        <a href='https://github.com/modlist-org' rel='noopener noreferrer' target='_blank'>{$_('pages.resources.modlist_link')}</a>
       </li>
     </ul>
   </div>
@@ -39,13 +40,11 @@
 <section class='prettier-tags'>
   <div class='content'>
     <h2>Prettier Tags</h2>
-    <p>Prettier Tags is an open-source script for the <a href='https://github.com/modlist-org/Overlayer' class='inline-link'>Overlayer</a> and the <a href='https://github.com/modlist-org/Overlayer.Scripting' class='inline-link'>Overlayer.Scripting</a> mod 
-      made for the game A Dance of Fire and Ice, 
-      it is based on iTags, Better iTags Alpha, Not Enough Tags, and maimai.js.</p>
-      <p>Made by team members <strong>nuomibinggao</strong> and <strong>MLob_302</strong>.</p>
+    <p>Prettier Tags{$_('pages.resources.prettier_tags_desc_1')}<a href='https://github.com/modlist-org/Overlayer' class='inline-link'>Overlayer</a>{$_('pages.resources.prettier_tags_desc_2')}<a href='https://github.com/modlist-org/Overlayer.Scripting' class='inline-link'>Overlayer.Scripting</a>{$_('pages.resources.prettier_tags_desc_3')}</p>
+    <p>{$_('pages.resources.prettier_tags_desc_4')}<strong>nuomibinggao</strong>{$_('pages.resources.prettier_tags_desc_5')}<strong>MLob_302</strong>{$_('pages.resources.prettier_tags_desc_6')}</p>
   </div>
   <div class='links'>
-    <a href='https://github.com/nuomibinggao/Prettier-Tags' rel='noopener noreferrer' target='_blank'>View on GitHub</a>
+    <a href='https://github.com/nuomibinggao/Prettier-Tags' rel='noopener noreferrer' target='_blank'>{$_('pages.resources.github_link')}</a>
   </div>
 </section>
 
